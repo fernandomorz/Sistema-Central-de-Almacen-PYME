de9503a73f2b4bc3192b488cc323aa3d02dae00d
FERNANDO MORENO RUIZ <76419541+fernandomorz@users.noreply.github.com>
2022-02-10 22:58:33 -0600

    Add files via upload

---
+++ Sistema_Inventario_Prestamo.cpp
@@ -0,0 +1,32 @@
+
+*#include <iostream>
+using namespace std;
+
+class Equipo{
+    private:
+        char nombre[];
+        char fabricante[];
+        char modelo[];
+        char accesorios_caja [];
+        char accesorios_stock[];
+        char ubicacion_stock[];
+        char palabras_clave[];
+        
+    public:
+        int  ;
+        int numero_de_usos;
+        char usuario[];
+        char destino[];
+}
+
+
+int main (){
+
+Solicitar Prestamo 
+    Quien
+    Ubicacion 
+    Que
+    
+
+    return 0;
+}*/
